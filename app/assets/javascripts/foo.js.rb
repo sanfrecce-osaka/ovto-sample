aa0ee96624787328d614348ecc861596a442bee4
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-27 18:51:50 +0900

    foo.js.rbを追加

---
+++ app/assets/javascripts/foo.js.rb
@@ -0,0 +1,15 @@
+require 'ovto'
+
+class Foo < Ovto::App
+  class State < Ovto::State
+  end
+
+  class Actions < Ovto::Actions
+  end
+
+  class MainComponent < Ovto::Component
+    def render(state:)
+      o 'h1', 'HELLO'
+    end
+  end
+end
