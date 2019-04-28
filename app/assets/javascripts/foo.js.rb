cb0be5d275a3d386de730178062b797b409d45f9
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-04-28 18:08:29 +0900

    ボタンをクリックするとランダムでボタンの背景色が変わる機能を追加

--- app/assets/javascripts/foo.js.rb
+++ app/assets/javascripts/foo.js.rb
@@ -1,15 +1,27 @@
 require 'ovto'
 
 class Foo < Ovto::App
+  COLORS = %w(red blue green pink yellow gray)
+
   class State < Ovto::State
+    item :color_index, default: 0
   end
 
   class Actions < Ovto::Actions
+    def update_color
+      new_index = (0..(COLORS.length - 1)).to_a.sample
+      { color_index: new_index }
+    end
   end
 
   class MainComponent < Ovto::Component
-    def render(state:)
-      o 'h1', 'HELLO'
+    def render
+      o 'input', {
+        type: 'button',
+        value: 'Hello',
+        style: { background: COLORS[state.color_index] },
+        onclick: -> { actions.update_color }
+      }
     end
   end
 end
