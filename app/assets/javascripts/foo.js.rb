16a14d546737c2e82937659cb163b1abcd6443fd
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-05-12 18:34:26 +0900

    アクションをactions.js.rbに外出し

--- app/assets/javascripts/foo.js.rb
+++ app/assets/javascripts/foo.js.rb
@@ -7,32 +7,6 @@ class Foo < Ovto::App
     item :additional_color, default: ''
   end
 
-  class Actions < Ovto::Actions
-    def update_rondom_color
-      { color_index: new_index }
-    end
-
-    def new_index
-      (0..(state.colors.length - 1)).to_a.sample
-    end
-
-    def update_designated_color(color_name:)
-      { color_index: state.colors.index(color_name) }
-    end
-
-    def dump_additional_color(value:)
-      { additional_color: value }
-    end
-
-    def add_color
-      { colors: set_colors, additional_color: '' }
-    end
-
-    def set_colors
-      state.additional_color.empty? ? state.colors : state.colors.push(state.additional_color)
-    end
-  end
-
   class MainComponent < Ovto::Component
     def render
       o '.ColorChanger' do
