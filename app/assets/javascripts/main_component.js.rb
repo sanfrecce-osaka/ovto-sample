0403aa51275afbc32101eb685ec693501679e141
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-05-12 20:25:57 +0900

    コンポーネントをmain_component.js.rbに外出し

---
+++ app/assets/javascripts/main_component.js.rb
@@ -0,0 +1,44 @@
+require 'ovto'
+
+class Foo < Ovto::App
+  class MainComponent < Ovto::Component
+    def render
+      o '.ColorChanger' do
+        o 'input', {
+          type: 'button',
+          value: 'Hello',
+          style: { background: state.color.colors[state.color.color_index] },
+          onclick: -> { actions.update_rondom_color }
+        }
+        o 'input', {
+          type: 'text',
+          value: state.color.colors[state.color.color_index],
+          disabled: 'disabled'
+        }
+        o 'label', { for: 'changing_color' }, 'designation: '
+        o 'select', {
+          id: 'changing_color',
+          value: state.color.colors[state.color.color_index],
+          onchange: -> (e) { actions.update_designated_color(color_name: e.target.value) }
+        } do
+          state.color.colors.each do |color|
+            o 'option', { value: color }, color
+          end
+        end
+        o 'label', { for: 'add_color' }, 'addition: '
+        o 'input', {
+          id: 'add_color',
+          type: 'text',
+          value: state.color.additional_color,
+          onchange: -> (e) { actions.dump_additional_color(value: e.target.value) }
+        }
+        o 'input', {
+          type: 'button',
+          value: 'add',
+          style: { background: 'purple' },
+          onclick: -> { actions.add_color }
+        }
+      end
+    end
+  end
+end
